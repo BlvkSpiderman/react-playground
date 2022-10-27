3fa820508ce1ce2bea4226fb995184e7a1e42e1c
Henry Assan <dadzie1921@gmail.com>
2022-10-27 18:46:14 +0000

    First Phase of development and reorganizing

---
+++ team-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+class App extends React.Component {
+    render() {
+
+        return (
+            <div>
+                <p>Hello there!</p>
+            </div>
+        );
+    }
+}
+
+export default App;
