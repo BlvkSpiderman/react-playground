28552c7c0e7818265c7c8b46111fbae24bdd7543
Henry Assan <dadzie1921@gmail.com>
2022-10-29 15:48:06 +0000

    Added content

--- team-app/src/Footer.jsx
+++ team-app/src/Footer.jsx
@@ -1,10 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
+    var today = new Date();
+    
     return (    
-        <footer>
-            <h1>Footer</h1>
-        </footer>
+    <footer className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h5>Team Member Allocation App - {today.getFullYear()}</h5>
+        </div>
+      </div>
+    </footer>
     );
 }
 
