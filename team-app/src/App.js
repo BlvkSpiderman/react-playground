a01d6268868d210070235f63f5b138c9788fcb9b
Henry Assan <dadzie1921@gmail.com>
2022-10-29 14:55:17 +0000

    State components moved to parent component

--- team-app/src/App.js
+++ team-app/src/App.js
@@ -1,19 +1,45 @@
 import React from "react";
+import {useState} from 'react';
 import './App.css';
 import Header from "./Header";
 import Employees from "./Employees";
 import Footer from "./Footer";
+import { EmployeeDatabase } from './EmployeeDatabase';
 
-class App extends React.Component {
-    render() {
+function App() {
+    
+    const [selectedTeam, setTeam] = useState('null');
+    const [employees, setEmployees] = useState(EmployeeDatabase);    
+
+    function handleTeamSelectionChange(event) {
+        console.log(event.target.value);
+        setTeam(event.target.value);
+    }
+
+    function handleEmployeeCardClick(event) { 
+        if(selectedTeam === 'null') {
+            return ;
+        }
+        const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
+        ? employee.teamName === selectedTeam ? { ...employee, teamName: '' }
+          : { ...employee, teamName: selectedTeam }: employee);
+      setEmployees(transformedEmployees);
+    }
+
+    
         return (
         <div>
-            <Header />
-            <Employees />
+            <Header 
+            selectedTeam={selectedTeam}
+            teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}/>
+            <Employees 
+            employees={employees}
+            selectedTeam={selectedTeam}
+            handleEmployeeCardClick={handleEmployeeCardClick}
+            handleTeamSelectionChange={handleTeamSelectionChange} />
             <Footer />
         </div>
         );
-    }
 }
 
 export default App;
