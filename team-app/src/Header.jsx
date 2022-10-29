d3c5c40e31bd5f3e9903a3e438b391be023b28a7
Henry Assan <dadzie1921@gmail.com>
2022-10-29 15:48:27 +0000

    Added content

--- team-app/src/Header.jsx
+++ team-app/src/Header.jsx
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Header = () => {
+const Header = ({selectedTeam, teamMemberCount}) => {
     return (    
-        <header>
-            <h1>Header</h1>
+        <header className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <div className="col-8">
+                    <h1>Team Member Allocation</h1>
+                    <h3> {selectedTeam === 'null'? 
+                        'Select team to view members' 
+                        : selectedTeam + ' has ' + teamMemberCount +' Members'}
+                    </h3>
+                </div>
+            </div>
         </header>
     );
-}
-
+}   
+/**
+ * {selectedTeam === 'null'? 'Select team to view members': '{selectedTeam} has {teamMemberCount} Members}
+ */
 export default Header;
