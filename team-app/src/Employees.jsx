fc51d1dfea5001aca7b7b8b6fbf80b569ee44585
Henry Assan <dadzie1921@gmail.com>
2022-10-29 14:54:24 +0000

    State dependent components moved

--- team-app/src/Employees.jsx
+++ team-app/src/Employees.jsx
@@ -1,46 +1,27 @@
-import {useState} from 'react';
 import React from 'react';
-import { EmployeeDatabase } from './EmployeeDatabase';
 import femaleProfile from './images/femaleProfile.jpg';
 import maleProfile from './images/maleProfile.jpg';
 
 
-const Employees = () => {
-    const [selectedTeam, setTeam] = useState('null');
-    const [employees, setEmployees] = useState(EmployeeDatabase);    
-
-    function handleTeamSelectionChange(event) {
-        console.log(event.target.value);
-        setTeam(event.target.value);
-    }
-
-    function handleEmployeeCardClick(event) { 
-        if(selectedTeam === 'null') {
-            return ;
-        }
-        const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
-        ? employee.teamName === selectedTeam ? { ...employee, teamName: '' }
-          : { ...employee, teamName: selectedTeam }: employee);
-      setEmployees(transformedEmployees);
-    }
+const Employees = (props) => {
 
     return (    
         <main className='container'>
             <div className="row justify-content-center mt-3 mb-3">
                 <div className='col-6'>
-                <select className="form-select form-select-lg" id="teams" value={selectedTeam} onChange={handleTeamSelectionChange}>
+                <select className="form-select form-select-lg" id="teams" value={props.selectedTeam} onChange={props.handleTeamSelectionChange}>
                     <option value="null" defaultValue={true}>Select Team</option>
-                    <option value="TeamA">Team A</option>
-                    <option value="TeamB">Team B</option>
-                    <option value="TeamC">Team C</option>
-                    <option value="TeamD">Team D</option>
+                    <option value="Team A">Team A</option>
+                    <option value="Team B">Team B</option>
+                    <option value="Team C">Team C</option>
+                    <option value="Team D">Team D</option>
                 </select>        
                 </div>
                 <div className="col-8">
                     <div className="card-collection">
                         {
-                            employees.map((employee) => (
-                                <div id={employee.id} className={(employee.teamName === selectedTeam? 'card m-2 standout': 'card m-2')} style={{cursor: 'pointer'}} onClick={handleEmployeeCardClick} >                            
+                            props.employees.map((employee) => (
+                                <div id={employee.id} key={employee.id.toString()} className={(employee.teamName === props.selectedTeam? 'card m-2 standout': 'card m-2')} style={{cursor: 'pointer'}} onClick={props.handleEmployeeCardClick} >                            
                                 {(employee.gender === 'male') ? <img src={maleProfile} alt='male profile pic' className="card-ing-top" />
                                                             :<img src={femaleProfile} alt='female profile pic' className="card-ing-top" />}                            
                                 <div className="card-body"> 
