e61900b6fb47eafd94199d6b7b00b444dd6649d8
Henry Assan <dadzie1921@gmail.com>
2022-11-07 00:14:32 +0000

    Adding contaxt wrap conponent

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -2,21 +2,23 @@ import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { StockOverviewPage } from './pages/StockOverviewPage';
 import { StockDetailPage } from './pages/StockDetailPage';
-
+import { WatchListContextProvider } from './context/WatchListContext';
 
 function App() {
   return (
    <main className='container'>
-    <BrowserRouter>
-      <Routes>
-        <Route 
-        path='/'
-        element = {<StockOverviewPage/>}/>
-        <Route 
-        path='/detail/:symbol'
-        element={<StockDetailPage/>}/>
-      </Routes>
-    </BrowserRouter>
+    <WatchListContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+          path='/'
+          element = {<StockOverviewPage/>}/>
+          <Route 
+          path='/detail/:symbol'
+          element={<StockDetailPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </WatchListContextProvider>
    </main>
   );
 }
