475458becaf75b13400d6807b792c2c1911a26c8
Henry Assan <dadzie1921@gmail.com>
2022-11-08 02:30:04 +0000

    new component

---
+++ stock-app/src/components/StockData.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import finnHub from "../apis/finnHub"
+
+export const StockData = ({ symbol }) => {
+const [stockData, setStockData] = useState([]);
+
+    useEffect(() => {
+        let isMounted = true;
+        
+        const fetchData = async () => {
+            try {
+                const response = await finnHub.get('/stock/profile2', {
+                    params: {
+                        symbol
+                    }
+                })
+                console.log(response)
+                if(isMounted) {
+                    setStockData(response.data);
+                }
+            } catch (error) {
+                
+            }
+        }
+        fetchData();
+        return () => (isMounted = false);
+    }, [symbol])
+    return (
+        <div>
+            {stockData && (
+                <div className="row border bg-white rounded shadow-sm p-4 mt-5">
+                    <div className="col">
+                        <div>
+                            <span className="fw-bold"></span>
+                        </div>
+                        <div>
+                            <span className="fw-bold"></span>
+                        </div>
+                        <div>
+                            <span className="fw-bold"></span>
+                        </div>
+                    <div className="col"></div>
+                    <div className="col"></div>
+                </div>
+            )}
+        </div>
+    );
+}
