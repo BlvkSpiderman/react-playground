6a38a7b5e1ebce6b1a45cd0b0230402ca3781fab
Henry Assan <dadzie1921@gmail.com>
2022-11-10 20:01:06 +0000

    adding click outside modal to close

--- meals-app/src/Components/Modal.jsx
+++ meals-app/src/Components/Modal.jsx
@@ -7,8 +7,8 @@ const Modal = () => {
     const {strMealThumb: image, strMeal: title, strInstructions: text, strSource: source } = selectedMeal;
     
     return(
-        <aside className="modal-overlay">
-            <div className="modal-container">
+        <aside className="modal-overlay" onClick={() => closeModal()}>
+            <div className="modal-container" onClick={(e) => e.stopPropagation()}>
                 <img src={image} alt={title} className="img modal-img" />
                 <div className="modal-content">
                     <h4>{title}</h4>
@@ -24,3 +24,54 @@ const Modal = () => {
 }
 
 export default Modal;
+
+/**
+ function Modal({ children, shown, close }) {
+  return shown ? (
+    <div
+      className="modal-backdrop"
+      onClick={() => {
+        // close modal when outside of modal is clicked
+        close();
+      }}
+    >
+      <div
+        className="modal-content"
+        onClick={e => {
+          // do not close modal if anything inside modal content is clicked
+          e.stopPropagation();
+        }}
+      >
+        <button onClick={close}>Close</button>
+        {children}
+      </div>
+    </div>
+  ) : null;
+}
+
+function App() {
+  const [modalShown, toggleModal] = React.useState(false);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <p>modalShown: {modalShown.toString()}</p>
+      <button
+        onClick={() => {
+          toggleModal(!modalShown);
+        }}
+      >
+        Toggle Modal
+      </button>
+      <Modal
+        shown={modalShown}
+        close={() => {
+          toggleModal(false);
+        }}
+      >
+        <h1>Look! I'm inside the modal!</h1>
+      </Modal>
+    </div>
+  );
+}
+ */
