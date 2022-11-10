8bd091aabbe563aaa96a7a9362d191edd31a2228
Henry Assan <dadzie1921@gmail.com>
2022-11-10 21:57:25 +0000

    completing the component

--- stock-app/src/components/StockData.jsx
+++ stock-app/src/components/StockData.jsx
@@ -14,10 +14,11 @@ const [stockData, setStockData] = useState([]);
                         symbol
                     }
                 })
-                console.log(response)
+
                 if(isMounted) {
                     setStockData(response.data);
                 }
+                
             } catch (error) {
                 
             }
@@ -25,24 +26,53 @@ const [stockData, setStockData] = useState([]);
         fetchData();
         return () => (isMounted = false);
     }, [symbol])
+    
     return (
-        <div>
-            {stockData && (
-                <div className="row border bg-white rounded shadow-sm p-4 mt-5">
-                    <div className="col">
-                        <div>
-                            <span className="fw-bold"></span>
-                        </div>
-                        <div>
-                            <span className="fw-bold"></span>
-                        </div>
-                        <div>
-                            <span className="fw-bold"></span>
-                        </div>
-                    <div className="col"></div>
-                    <div className="col"></div>
+        <div>  
+            <div style={{marginBottm: '50px'}} className="row border bg-white rounded shadow-sm p-4 mt-5">
+                <div className="col">
+                    <div>
+                        <span className="fw-bold">name: </span>
+                        {stockData.name}
+                    </div>
+                    <div>
+                        <span className="fw-bold">country: </span>
+                        {stockData.country}
+                    </div>
+                    <div>
+                        <span className="fw-bold">ticker: </span>
+                        {stockData.ticker}
+                    </div>
                 </div>
-            )}
-        </div>
+                <div className="col">
+                    <div>
+                        <span className="fw-bold">Exchange: </span>
+                        {stockData.exchange}
+                    </div>
+                    <div>
+                        <span className="fw-bold">Industry: </span>
+                        {stockData.finnhubIndustry}
+                    </div>
+                    <div>
+                        <span className="fw-bold">IPO: </span>
+                        {stockData.ipo}
+                    </div>
+                </div>
+                <div className="col">
+                    <div>
+                        <span className="fw-bold">MarketCap: </span>
+                        {stockData.marketCapitalization}
+                    </div>
+                    <div>
+                        <span className="fw-bold">Shares Outstanding: </span>
+                        {stockData.shareOutstanding}
+                    </div>
+                    <div>
+                        <span className="fw-bold">url: </span>
+                        <a href={stockData.weburl} target='_blank' rel="noreferrer">{stockData.weburl}</a>
+                    </div>
+                </div>
+            </div>
+      </div >
     );
 }
