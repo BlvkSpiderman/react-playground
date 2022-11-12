bd1aaf4e50732c4e96e9be7068fe110626bb1058
Henry Assan <dadzie1921@gmail.com>
2022-11-12 03:53:05 +0000

    adding elements

---
+++ team-app-2.0/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { useEmployeeContext } from '../context/EmployeeContext';
+
+const Header = () => {
+    const { selectedTeam, teamMemberCount } = useEmployeeContext();
+    return (    
+        <header className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <div className="col-8">
+                    <h1>Team Member Allocation</h1>
+                    <h3> {selectedTeam === 'null'? 
+                        'Select team to view members' 
+                        : selectedTeam + ' has ' + teamMemberCount +' Members'}
+                    </h3>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ team-app-2.0/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+export const Nav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to='/'>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/teams'>Teams</Link>
+            </li>
+          </ul>
+        </nav>  
+    )
+}
+
+export default Nav;
