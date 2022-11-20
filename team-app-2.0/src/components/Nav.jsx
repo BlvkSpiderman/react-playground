9d876f9bdc80e61825cba5573e4c85ed989f8b8d
Henry Assan <dadzie1921@gmail.com>
2022-11-20 19:56:44 +0000

    nav bar restyling

--- team-app-2.0/src/components/Nav.jsx
+++ team-app-2.0/src/components/Nav.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 export const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-          <ul className="mb-2 mb-lg-0 " style={{width: '18vw', display: 'flex', flexFlow:'row', justifyContent: 'space-around', listStyle: 'none', margin: '0 auto'}}>
+          <ul className="mb-2 mb-lg-0 " 
+          style={{width: '18vw', display: 'flex', flexFlow:'row', justifyContent: 'space-around', listStyle: 'none', margin: '0 auto'}}>
             <li className="nav-item">
               <Link className="nav-link" to='/'>Home</Link>
             </li>
