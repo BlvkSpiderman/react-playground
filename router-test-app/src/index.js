01a936cdbf9c7819344e5f7a83c5dc8400587a1b
Henry Assan <dadzie1921@gmail.com>
2022-12-18 19:38:38 +0000

    Additional routes and functions

--- router-test-app/src/index.js
+++ router-test-app/src/index.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Root from './routes/root';
+import Root, { 
+  loader as rootLoader,
+  action as rootAction 
+} from './routes/root';
 import {  createBrowserRouter,  RouterProvider } from "react-router-dom";
 import ErrorPage from './routes/error-page';
-import Contact from './routes/contact';
+import Contact, { 
+  loader as contactLoader,
+  action as contactAction 
+} from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
+import Index from './routes';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root/>,
     errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
-    ],
-  },
+        errorElement: <ErrorPage/>,
+        children: [
+          { 
+            index: true, 
+            element: <Index /> 
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact/>,
+            loader: contactLoader,
+            action: editAction
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction
+          }
+        ]
+      }
+    ]
+  }
 ]);
 
 
